fe4e7006237d7a69b39f0121c847c258f0acd4d7
Felix Oertel <felix_oertel402@hotmail.com>
2020-03-13 13:03:50 +0000

    First commit

---
+++ CM50270/week 2/utils.py
@@ -0,0 +1,10 @@
+# This file is imported from within the rl0_python_intro.ipynb Jupyter notebook.
+# You can write code here that you do not want to show in your notebook.
+
+
+def utils_test():
+    print("This string was printed by the function 'utils_test()' as defined in the file utils.py")
+
+
+def another_function():
+    print("This string was printed by the function 'another_function()' as defined in the file utils.py")
